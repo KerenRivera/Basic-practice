a93d4d57fdfe00221ff655665b93a423d965b943
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-10-22 20:28:37 -0400

    Cars Assemble exercise from Exercism, completed

---
+++ Cars Assemble/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int speed = 5;
+        Console.WriteLine($"Success Rate for speed {speed}: {AssemblyLine.SuccessRate(speed)}");
+        Console.WriteLine($"Production Rate per Hour for speed {speed}: {AssemblyLine.ProductionRatePerHour(speed)}");
+        Console.WriteLine($"Working Items per Minute for speed {speed}: {AssemblyLine.WorkingItemsPerMinute(speed)}");
+    }
+}
+
