4998230032371a10d957581e29e21a88d0110cdf
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-10-22 20:50:20 -0400

    Annalyn's Infiltration exercise from Exercism, completed

---
+++ Annalyn's Infiltration/Annalyn'sInfiltration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Annalyn_s_Infiltration
+{
+    static class AnnalynsInfiltration
+    {
+        public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
+
+        public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
+
+            knightIsAwake || archerIsAwake || prisonerIsAwake;
+
+        public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
+
+            !archerIsAwake && prisonerIsAwake;
+
+        public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) =>
+
+         (petDogIsPresent && !archerIsAwake) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake);
+
+    }
+}
